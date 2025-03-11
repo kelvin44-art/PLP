0c2a95eba37698d2f2264026492712be2b230c56
kelvin44-art <waruingekelvin@gmail.com>
2025-03-12 00:40:37 +0300

    first commit

--- asssignment_one.py
+++ asssignment_one.py
@@ -1,6 +1,11 @@
-num1 = print(input("enter the value of the first number"))
-operation = print(input("enter the operation(+,-,*,/,%)"))
-num2 = print(input("enter the value of the second number"))
+// Create a simple Python program that asks the user to input two numbers and 
+// a mathematical operation (addition, subtraction, multiplication, or division).
+// Perform the operation based on the user's input and print the result.
+
+
+num1 = print(input("enter the value of first number"))
+operation = print("enter the opetion ")
+num2 = print(input("enter the value of second number"))
 
 if operation == '+':
     print(num1 + num2)
@@ -10,7 +15,6 @@ elif operation == '*':
     print(num1 * num2)
 elif operation == '/':
     print(num1 / num2)
-elif operation == '%':
-    print(num1 % num2)
 else:
-    print("enter a valid operation and/or values")
+    print("enter a valid operand")
+
