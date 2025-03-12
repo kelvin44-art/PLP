7db23ffacd623e026e96cad38dc29277239fcb1b
kelvin44-art <waruingekelvin@gmail.com>
2025-03-12 21:37:39 +0300

    Assignment two

--- asssignment_one.py
+++ asssignment_one.py
@@ -1,6 +1,6 @@
-// Create a simple Python program that asks the user to input two numbers and 
-// a mathematical operation (addition, subtraction, multiplication, or division).
-// Perform the operation based on the user's input and print the result.
+#Create a simple Python program that asks the user to input two numbers and 
+# a mathematical operation (addition, subtraction, multiplication, or division).
+# Perform the operation based on the user's input and print the result.
 
 
 num1 = print(input("enter the value of first number"))
